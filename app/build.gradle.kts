ea9a986a4a91ecc4b532373058e25551c7b0a27c
lisalylo <11037798@stud.hochschule-heidelberg.de>
2025-06-12 23:26:03 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,5 @@ dependencies {
     implementation(libs.barcode.scanning.v1722)
     implementation(libs.guava)
     implementation(libs.jetbrains.kotlinx.coroutines.guava)
-
-    //ZXing Core (QR-Code-Generierung)
     implementation(libs.zxing.core)
 }
